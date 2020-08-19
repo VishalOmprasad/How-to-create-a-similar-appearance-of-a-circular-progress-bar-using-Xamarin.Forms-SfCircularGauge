9bcf728e5273b2aa800f174e56ab343b327a82de
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-08-19 17:31:39 +0530

    Sample included

---
+++ GaugeSample/GaugeSample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace GaugeSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Timer timer = new Timer();
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(1.5), () =>
+            {
+                if (rangePointer.Value>=100)
+                {
+                    rangePointer.Value = 0;
+                }
+                rangePointer.Value = rangePointer.Value+20;
+
+                 return true;
+            });
+        }
+    }
+}
